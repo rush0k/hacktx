33b184355d1fc7c4d6dbd978f7900283d384eb33
rush0k <55649042+rush0k@users.noreply.github.com>
2020-10-25 04:35:05 -0500

    Add files via upload

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -2,20 +2,3 @@
 $(document).ready(function() {
   $('#dataTable').DataTable();
 });
-$(document).ready(function() {
-  var table = $('#example').DataTable({
-      columnDefs: [{
-          orderable: false,
-          targets: [1,2,3]
-      }]
-  });
-
-  $('button').click( function() {
-      var data = table.$('input, select').serialize();
-      alert(
-          "The following data would have been submitted to the server: \n\n"+
-          data.substr( 0, 120 )+'...'
-      );
-      return false;
-  } );
-} );
